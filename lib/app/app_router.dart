7670e2189e8d6789b387370cb3b66bf2a76cb6cc
EMSCodeSim <davidagallaher@hotmail.com>
2026-07-16 11:33:24 -0600

    Add files via upload

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../screens.dart';
+import 'app_controller.dart';
 
 GoRouter buildRouter({required AppController controller}) {
   return GoRouter(
